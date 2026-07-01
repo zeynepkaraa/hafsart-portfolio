24436b151ff8305321ea0e3b31ef887141d6d5a1
zeynepkaraa <zeynepkara.tmu@gmail.com>
2026-07-01 00:45:40 -0400

    include sitemap

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,25 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: "https://www.hafsart.com",
+      dynamicRoutes: [
+        "/",
+        "/portfolio",
+        "/about",
+        "/book",
+        "/live",
+        "/custom",
+        "/wedding",
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
